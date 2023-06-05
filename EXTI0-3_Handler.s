42a67a7e70e77b512536d7df99b58d86545ece4a
michellebrady08 <110513243+michellebrady08@users.noreply.github.com>
2023-06-05 06:01:06 -0600

    Update EXTI0-3_Handler.s

--- EXTI0-3_Handler.s
+++ EXTI0-3_Handler.s
@@ -9,16 +9,16 @@
 .global EXTI0_3_Handler
 
 EXTI0_3_Handler:
+    ldr     r0, =EXTI_BASE
+    ldr     r0, [r0, EXTI_PR_OFFSET]
+    ldr     r1, =0x1
+    cmp     r0, r1                  @ if EXTI 0 bit is set
+    beq     EXTI0_Handler
+    ldr     r1, =0x8                @ if EXTI 3 bit is set
+    cmp     r0, r1
+    beq     EXTI3_Handler
+    bx      lr
 
-        ldr     r0, =EXTI_BASE
-        ldr     r0, [r0, EXTI_PR_OFFSET]
-        ldr     r1, =0x1
-        cmp     r0, r1                  @ if EXTI 0 bit is on
-        beq     EXTI0_Handler
-        ldr     r1, =0x8                @ if EXTI 3 bit is on
-        cmp     r0, r1
-        beq     EXTI3_Handler
-        bx      lr
 // If button 0 is pressed we speed the systick interrupt frequency
 .size   EXTI0_3_Handler, .-EXTI0_3_Handler
 
@@ -28,25 +28,26 @@ EXTI0_3_Handler:
 .thumb
 .global EXTI0_Handler
 EXTI0_Handler:
-        
         ## if(speed != 8)
         mov     r1, r5
-        ldr     r2, =8
-        cmp     r1, r2             
+        cmp     r1, #8             
         beq     x1   
         ## speed = speed*2          
-        lsls    r1, r1, r1
+        lsls    r1, r1, #1
         mov     r5, r1
         b       end
         ## else
 x1:
         ## speed = 1;
-        ldr     r5, =1
+        mov    r5, #1
 end:
-        ldr r0, =EXTI_BASE
-        ldr r1, =0x1
-        str r1, [r0, EXTI_PR_OFFSET]
+        
+        ldr     r0, =EXTI_BASE
+        ldr     r1, [r0, EXTI_PR_OFFSET]
+        orr     r1, r1, 0x40
+        str     r1, [r0, EXTI_PR_OFFSET]
         bx      lr
+
  .size   EXTI0_Handler, .-EXTI0_Handler
 
 # r6 -> holds mode 
@@ -56,15 +57,25 @@ end:
 .thumb
 .global EXTI3_Handler
 EXTI3_Handler:
-        
+
+        ## if (mode!=0)
         mov     r1, r6
-        eor     r1, 1
-        and     r1, 0x1
+        cmp     r1, 0x0
+        bne     _positive
+        mov     r1, 0x0
+        b       _end
+_positive:
+        mov     r1, 0x1
+_end:
         mov     r6, r1
-        ldr r0, =EXTI_BASE
-        ldr r1, =8
-        str r1, [r0, EXTI_PR_OFFSET]
+        ldr     r0, =EXTI_BASE
+        #ldr     r1, =8          @0x240
+        ldr     r2, [r0, EXTI_PR_OFFSET]
+        orr     r2, r2, 0x200             @ Clear the EXTI3 pending flag
+        str     r2, [r0, EXTI_PR_OFFSET]
+ 
         bx      lr 
 
 .size   EXTI3_Handler, .-EXTI3_Handler
+
 
