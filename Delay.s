bfe93004d97bedb5fd1d9e4984767cb8ee1f7556
michellebrady08 <110513243+michellebrady08@users.noreply.github.com>
2023-06-04 20:00:17 -0600

    Update Delay.s

--- Delay.s
+++ Delay.s
@@ -13,9 +13,7 @@ delay:
         push    {r7} 
         sub     sp, sp, #4 
         add     r7, sp, #0 
-        #str     r0, [r7]
-        #ldr     r10, [r7]
-        mov     r10, r0 
+        mov     r10, r0
 delay_loop:
         cmp     r10, #0
         bne     delay_loop
